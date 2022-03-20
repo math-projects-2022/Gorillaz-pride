d4909b3fb18a6a7de59d98b247717ed2cff6263b
Leonid <39415744+leonrew@users.noreply.github.com>
2022-03-20 15:45:53 +0700

    Add files via upload

---
+++ main.h
@@ -0,0 +1,6 @@
+#pragma once
+double f(double);
+
+void trapezoidalIntegral(double, double, double, double);
+void centralrectanglesIntegral(double, double, double, double);
+void SimpsonIntegral(double, double, double, double);
