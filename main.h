5b7b566acc9f0e2179329429da5b904e168e5ee6
Leonid <39415744+leonrew@users.noreply.github.com>
2022-03-21 00:38:47 +0700

    Delete main.h

--- main.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-double f(double);
-
-void trapezoidalIntegral(double, double, double, double);
-void centralrectanglesIntegral(double, double, double, double);
-void SimpsonIntegral(double, double, double, double);
